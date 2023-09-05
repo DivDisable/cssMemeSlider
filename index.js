f1524ff75f8426a7ec9437ea9e10ab3c2e269979
Ilya <millerite96@gmail.com>
2023-09-06 01:23:41 +0600

    fix: remove position absolute (Wed, Sep 6, 2023 01:24 AM)

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 const inputs = document.querySelectorAll("input");
 const gallery = document.querySelector(".slider__gallery");
 const imgWidth = document.querySelector("img").width;
+const sliderTitle = document.querySelector(".slider__title");
 let currentImgNumber = Number(document.querySelector("input:checked").value);
+
+const titles = [
+  "capitalism",
+  "karl marx",
+  "html vs html + css",
+  "illusion of choice"
+];
+
+sliderTitle.innerHTML = titles[currentImgNumber];
+
+const hideSliderTitle = () => {
+  sliderTitle.classList.add("opacity-0");
+};
+
+const showSliderTitle = () => {
+  sliderTitle.classList.remove("opacity-0");
+  gallery.addEventListener("scroll", hideAndSetSliderTitle);
+};
+
+const setTitle = (titleNumber) => {
+  sliderTitle.innerHTML = titles[titleNumber];
+};
+
+const hideAndSetSliderTitle = () => {
+  gallery.removeEventListener("scroll", hideAndSetSliderTitle);
+  const transitionendHandle = () => {
+    setTitle(currentImgNumber);
+    sliderTitle.removeEventListener("transitionend", transitionendHandle);
+  };
+  sliderTitle.addEventListener("transitionend", transitionendHandle);
+  hideSliderTitle();
+};
+
+gallery.addEventListener("scroll", hideAndSetSliderTitle);
+gallery.addEventListener("scrollend", showSliderTitle);
+
 
 const moveSlider = (direction) => {
   if (direction === "left") {
